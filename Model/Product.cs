56eb4e7d82aa55a91522af97e2b45d578d04fcf2
FaiTh725 <sasha.zelenukho.2016@mail.ru>
2024-03-20 17:12:23 +0300

    перед попыткой создать свзять One to many

---
+++ Model/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Model
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("nvarchar(20)")]
+        public string Name { get; set; }
+
+        [Column("nvarchar(100)")]
+        public string? Description { get; set; }
+
+        [Column("nvarchar(20)")]
+        public string? ShortDescription { get; set; }
+
+        public double Price { get; set; }
+    }
+}
