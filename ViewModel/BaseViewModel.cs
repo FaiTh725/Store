cb58bf48dde6ce61727a73933bd324ac4ce67e35
FaiTh725 <sasha.zelenukho.2016@mail.ru>
2024-03-20 17:00:07 +0300

    Add project files.

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Props
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region INPC
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        #endregion
+    }
+}
